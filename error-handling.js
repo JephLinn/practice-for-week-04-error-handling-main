0791cc1f5db477a31f6916cae771293f6ef7b9f9
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-01-01 13:16:07 -0500

    Done

--- error-handling.js
+++ error-handling.js
@@ -7,15 +7,36 @@ function sum(array) {
   return sum;
 }
 
-let res = sum(null);
+try{ let res = sum(null);
 console.log(res);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.error(`Wrong Type: ${error.message}`);
+  } else {
+    console.error(error.message);
+  }
+}
+
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
 
+// Your code here
+sayName = (name) => {
+  if (typeof name !== String) {
+    throw new Error(`Invalid name! Must be a string!`)
+  } else {
+    console.log(name);
+  }
+}
+
+try {
+  sayName("Alex");
+sayName(1);
+} catch (error) {
+  console.error(error.message)
+
+}
 // 3.
 function greet(greeting) {
   if (!greeting) {
@@ -23,4 +44,12 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet()
+} catch (error) {
+  console.error(error.message);
+} finally {
+  console.log("Hello, World!")
 }
